8a9f68413b4c513a8fae4a5307072d98d663e276
rashmitha-converj <152256974+rashmitha-converj@users.noreply.github.com>
2024-01-23 16:44:40 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!\n")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is running on http://0.0.0.0:3001")
+	http.ListenAndServe("0.0.0.0:3001", nil)
+}
