dc58a102f9e70d25d9d4773cd3bb0f6b00572329
rashmitha-converj <152256974+rashmitha-converj@users.noreply.github.com>
2024-01-25 14:43:35 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, World! Rashmitha \n")
+	fmt.Fprint(w, "Hello, World! Rashmitha to Usha \n")
 }
 
 func main() {
